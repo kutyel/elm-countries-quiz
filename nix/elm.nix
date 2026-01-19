9e929029532bc848665c3a9343dff2dab30e21bd
Dwayne Crooks <me@dwaynecrooks.com>
2026-01-19 04:57:19 -0400

    Reuse installPatchScript in elm.nix
    I also checked that `result/elm-countries-quiz.js` contained
    `_VirtualDom_createTNode` to verify that the patching worked
    and it did contain it.

--- nix/elm.nix
+++ nix/elm.nix
@@ -1,35 +1,41 @@
-{ buildElmApplication, lib }:
+{ buildElmApplication, callPackage, lib }:
 
 let
   fs = lib.fileset;
-in
-buildElmApplication {
-  name = "elm-countries-quiz";
-  src = fs.toSource {
-    root = ../.;
-    fileset = fs.unions [
-      ../review
-      ../src
-      ../tests
-      ../elm.json
-    ];
+
+  drv = buildElmApplication {
+    name = "elm-countries-quiz";
+    src = fs.toSource {
+      root = ../.;
+      fileset = fs.unions [
+        ../review
+        ../src
+        ../tests
+        ../elm.json
+      ];
+    };
+    elmLock = ../elm.lock;
+    output = "elm-countries-quiz.js";
+
+    doElmFormat = true;
+    elmFormatSourceFiles = [ "review/src" "src" "tests" ];
+
+    doElmTest = true;
+
+    #
+    # To get this to work you have to generate the lock file as follows:
+    #
+    # elm2nix lock elm.json review/elm.json
+    #
+    doElmReview = true;
+
+    #
+    # Learn more: https://dev.to/dwayne/announcing-dwayneelm2nix-46pc
+    #
   };
-  elmLock = ../elm.lock;
-  output = "elm-countries-quiz.js";
 
-  doElmFormat = true;
-  elmFormatSourceFiles = [ "review/src" "src" "tests" ];
-
-  doElmTest = true;
-
-  #
-  # To get this to work you have to generate the lock file as follows:
-  #
-  # elm2nix lock elm.json review/elm.json
-  #
-  doElmReview = true;
-
-  #
-  # Learn more: https://dev.to/dwayne/announcing-dwayneelm2nix-46pc
-  #
-}
+  esvdPatches = callPackage ./elm-safe-virtual-dom/patches.nix {};
+in
+drv.overrideAttrs (old: {
+  prepareElmHomePhase = old.prepareElmHomePhase + esvdPatches;
+})
